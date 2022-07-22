65ca85147c9b2a1ca527ba1ba767024bd54389d4
Ashley Thompson <ashleythompson@Ashleys-MacBook-Air.local>
2022-07-22 17:48:46 -0400

    seeds.sql skeleton

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO department (id, dept_name)
+VALUES (),
+       (),
+       (),
+       (),
+       (),
+       (),
+       ();
+
+INSERT INTO  (id, title, salary)
+VALUES (),
+       (),
+       (),
+       (),
+       (),
+       (),
+       (); 
+
+INSERT INTO employees (id, first_name, last_name)
+VALUES (),
+       (),
+       (),
+       (),
+       (),
+       (),
+       (),
+       ();  
