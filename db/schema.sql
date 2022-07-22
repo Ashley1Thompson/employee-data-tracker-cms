f4cfaace3513bf0e0cfde387a1a10a26d3159cab
Ashley Thompson <ashleythompson@Ashleys-MacBook-Air.local>
2022-07-22 17:20:56 -0400

    finish schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,29 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
-USE movies_db;
+USE company_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
+  id INT NOT NULL PRIMARY KEY,
   dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE jobrole (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
-    ON DELETE SET NULL
+  id INT NOT NULL PRIMARY KEY,
+  title VARCHAR(30),
+  SALARY DECIMAL(10,2),
+  FOREIGN KEY (department_id),
+  REFERENCES department(id),
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  FOREIGN KEY (role_id),
+  REFERENCES jobrole(id),
+  FOREIGN KEY (manager_id),
+  REFERENCES employee(id),
+  ON DELETE SET NULL
 );
